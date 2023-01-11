d23ee10437a45893efaf3656a9312d337e1d15e7
eugene <81062608+eugene225@users.noreply.github.com>
2023-01-12 00:08:02 +0900

    jpa 게시판 조회
    [#5] jpa를 이용해 게시판 조회하기
    * 버튼 오른쪽 정렬 안됨

--- myspringsecurity/src/main/java/com/spring/myspringsecurity/controller/BoardController.java
+++ myspringsecurity/src/main/java/com/spring/myspringsecurity/controller/BoardController.java
@@ -1,15 +1,27 @@
 package com.spring.myspringsecurity.controller;
 
+import com.spring.myspringsecurity.model.Board;
+import com.spring.myspringsecurity.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")  //해당 컨트롤러의 전체 url
 public class BoardController {
 
+    @Autowired
+    BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
+
         return "board/list";
     }
 
