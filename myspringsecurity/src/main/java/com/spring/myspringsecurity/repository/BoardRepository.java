75d43b7f387a0e60507055dfe5c643b1f6942094
eugene <81062608+eugene225@users.noreply.github.com>
2023-03-10 00:40:25 +0900

    #8 끝!!!

--- myspringsecurity/src/main/java/com/spring/myspringsecurity/repository/BoardRepository.java
+++ myspringsecurity/src/main/java/com/spring/myspringsecurity/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.spring.myspringsecurity.repository;
 
 import com.spring.myspringsecurity.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +12,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findByTitle(String title);
 
     List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 
 }
