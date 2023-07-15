ae5655a09e2fc46f4266e7d284e2d83cdd880fd4
eugene <81062608+eugene225@users.noreply.github.com>
2023-07-15 15:45:00 +0900

    end

--- myspringsecurity/src/main/java/com/spring/myspringsecurity/controller/AccountController.java
+++ myspringsecurity/src/main/java/com/spring/myspringsecurity/controller/AccountController.java
@@ -2,12 +2,14 @@ package com.spring.myspringsecurity.controller;
 
 import com.spring.myspringsecurity.model.User;
 import com.spring.myspringsecurity.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/account")
 public class AccountController {
